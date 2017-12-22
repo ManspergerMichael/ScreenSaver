908cb5abe3b81d85947443749deed3965712101e
Michael_M <ManspergerMichael@gmail.com>
2017-12-22 15:53:46 -0800

    Add project files.

---
+++ ScreenSaver/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ScreenSaver
+{
+    partial class frmScreenSaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmScreenSaver";
+            this.ShowInTaskbar = false;
+            this.Text = "ScreenSaver";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmScreenSaver_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmScreenSaver_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmScreenSaver_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenSaver/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class frmScreenSaver : Form
+    {
+        //image objects
+        List<Image> BGImages = new List<Image>();
+        //location objects
+        List<BritPic> BritPics = new List<BritPic>();
+        Random rand = new Random();
+
+        //this class will instatiate a location object for the pictures
+        class BritPic
+        {
+            public int PicNum;
+            public float x;
+            public float y;
+            public float Speed;
+        }
+
+
+        public frmScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void frmScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+
+        private void frmScreenSaver_Load(object sender, EventArgs e)
+        {
+            string[] images = System.IO.Directory.GetFiles("Images");
+
+            //take all files in the directory and add them to List<Image>
+            foreach (string image in images)
+            {
+                BGImages.Add(new Bitmap(image));
+            }
+
+            //create 50 images on screen
+            for(int ii=0; ii <50; ii++)
+            {
+                BritPic mp = new BritPic();
+                mp.PicNum = ii % BGImages.Count;
+                mp.x = rand.Next(0, Width);
+                mp.y = rand.Next(0, Height);
+                                
+                BritPics.Add(mp);
+            }
+        }
+        //refreshes the screen after an interval
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void frmScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (BritPic bp in BritPics)
+            {
+                e.Graphics.DrawImage(BGImages[bp.PicNum], bp.x, bp.y);
+                bp.x -= 2;
+
+                if (bp.x < -250)
+                {
+                    bp.x = Width + rand.Next(20, 100);
+                }
+            }
+        }
+    }
+}
